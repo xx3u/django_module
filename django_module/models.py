45ee739b9838ef9d605d593930417aeb80a8bff9
xxuuu <yuliyahvan@yahoo.com>
2019-04-11 22:41:46 +0600

    back to master

--- django_module/models.py
+++ django_module/models.py
@@ -50,11 +50,6 @@ class Order(models.Model):
         blank=True,
         null=True
     )
-    customer = models.ForeignKey(
-        Customer,
-        on_delete=models.CASCADE,
-        related_name='orders'
-    )
     is_paid = models.BooleanField(default=False)
 
     def process(self):
@@ -73,6 +68,12 @@ class Order(models.Model):
         self.price = sum(
             (item.product.price * item.quantity for item in self.items.all())
         )
+
+        confirmed_payments = self.payments.filter(is_confirmed=True)
+        paid_amount = sum((payment.amount for payment in confirmed_payments))
+        if paid_amount < self.price:
+            raise Exception('Not enough money')
+
         self.is_paid = True
         self.save()
 
@@ -88,3 +89,16 @@ class OrderItem(models.Model):
         related_name='order_items'
     )
     quantity = models.IntegerField()
+
+
+class Payment(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='payments'
+    )
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        blank=True, null=True
+    )
+    is_confirmed = models.BooleanField(default=False)
--- django_module/tests/functional/test_order.py
+++ django_module/tests/functional/test_order.py
@@ -1,7 +1,7 @@
 import pytest
 
 from django_module.models import (
-    Order, OrderItem, Product, Store, StoreItem, Customer)
+    Order, OrderItem, Product, Store, StoreItem, Customer, Payment)
 
 
 @pytest.fixture
@@ -23,30 +23,42 @@ def data():
     )
     order = Order.objects.create(
         location='Almaty',
-        customer=customer
     )
     order_item = OrderItem.objects.create(
         order=order,
         product=product,
         quantity=10
     )
-    return product, store, store_item, customer, order, order_item
+    payment = Payment.objects.create(
+        order=order,
+        amount=1000,
+        is_confirmed=True
+    )
+    return product, store, store_item, customer, order, order_item, payment
 
 
 def test_order_process_is_ok(db, data):
-    product, store, store_item, customer, order, order_item = data
+    product, store, store_item, customer, order, order_item, payment = data
     order.process()
     store_item.refresh_from_db()
     assert order.price == 100
     assert order.is_paid is True
     assert store_item.quantity == 90
-    assert order.customer.name is 'John'
 
 
 def test_order_process_quantity_if_order_more_than_store(db, data):
-    product, store, store_item, customer, order, order_item = data
+    product, store, store_item, customer, order, order_item, payment = data
     order_item.quantity = 200
     order_item.save()
     with pytest.raises(Exception) as e:
         order.process()
     assert str(e.value) == 'Not enough stock'
+
+
+def test_order_process_payment_if_amount_less_than_order(db, data):
+    product, store, store_item, customer, order, order_item, payment = data
+    payment.amount = 10
+    payment.save()
+    with pytest.raises(Exception) as e:
+        order.process()
+    assert str(e.value) == 'Not enough money'
