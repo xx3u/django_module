ed4711ec332abfe7a4f26e47a967d1cd02c78d7d
xxuuu <yuliyahvan@yahoo.com>
2019-04-14 14:15:25 +0600

    Added admin site setting list of display for Product, Store, Order and using interface: tabular & stacked inlines

--- django_module/models.py
+++ django_module/models.py
@@ -6,13 +6,17 @@ class Product(models.Model):
     price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        blank=True,
-        null=True
     )
+
+    def __str__(self):
+        return self.name[:50]
 
 
 class Store(models.Model):
-    location = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.location[:50]
 
 
 class StoreItem(models.Model):
@@ -28,6 +32,9 @@ class StoreItem(models.Model):
     )
     quantity = models.IntegerField()
     location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.product.name[:50]
 
 
 class Customer(models.Model):
@@ -90,8 +97,12 @@ class OrderItem(models.Model):
     )
     quantity = models.IntegerField()
 
+    def __str__(self):
+        return self.product.name[:50]
+
 
 class Payment(models.Model):
+
     METHOD_CARD = 'card'
     METHOD_CASH = 'cash'
     METHOD_QIWI = 'qiwi'
