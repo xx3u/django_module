3c0fc655bc7bc870374bcc33b9d2c0140260932d
xxuuu <yuliyahvan@yahoo.com>
2019-06-21 13:26:14 +0600

    Use ModelForm and render it as a form instead of a bunch of inputs

--- django_module/views.py
+++ django_module/views.py
@@ -40,8 +40,11 @@ def order(request, order_id):
             )
         else:
             return HttpResponse('Validation error', status=400)
+    else:
+        form = OrderItemForm()
     return render(request, 'order.html', context={
-        'order': order
+        'order': order,
+        'form': form
     })
 
 
